cc47554568c996d4189dbd89df6daa4d2eb0e9f2
limsungju <fullmoon4860@gmail.com>
2019-08-06 16:39:11 +0900

    3번문제 완료

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,34 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		
+		int num = sc.nextInt();
+		
+		int val = 0;
+		
+		
+		if(num%2==0) {
+			for(int i=2; i<=num; i++) {
+				if(i%2==0) {
+					val = val + i;
+				}
+			}
+			System.out.println("결과 값 : " + val);
+		} else {
+			for (int j=1; j<=num; j++) {
+				if(j%2!=0) {
+					val = val + j;
+				}
+			}
+			System.out.println("결과 값 : " + val);
+		}
+		
+		
 	}
 }
