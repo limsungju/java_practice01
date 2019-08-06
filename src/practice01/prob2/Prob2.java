61bd5257bb8ad73fb775b54f266ac76b48c76ae5
limsungju <fullmoon4860@gmail.com>
2019-08-06 16:13:09 +0900

    2번문제 완료

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -2,5 +2,15 @@ package practice01.prob2;
 
 public class Prob2 {
 	public static void main (String args[]) {
+		int num = 0;
+		
+		for(int i=0; i<9; i++) {
+			for(int j=1; j<11; j++) {
+				num = i + j;
+				System.out.print(num + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 }
