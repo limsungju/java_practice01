3bbbedecaeff7aab2e26b34b3f305184cb9413c8
limsungju <fullmoon4860@gmail.com>
2019-08-06 16:06:31 +0900

    1번문제 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -1,6 +1,23 @@
 package practice01.prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요 : ");
+		int number = sc.nextInt();
+		
+		System.out.println(number);
+		
+		sc.close();
+		
+		if(number%3==0) {
+			System.out.println("3의 배수입니다.");
+		} else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+		
 	}
 }
